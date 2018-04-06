901b3c5c8739aa81fc7cb2c96cf969ebba7167f1
Jon Ward <jon@techdecaf.com>
2018-04-06 09:33:48 -0700

    simplify exports

--- _source.sh
+++ _source.sh
@@ -1,5 +1,6 @@
-export NAME=$(cat package.json | jq .name -r)
-export VERSION=$(cat package.json | jq .version -r)
-export REPO=$(cat package.json | jq .docker.repo -r)
-export CURRENT_TAG=$REPO/$NAME:$VERSION
-export LATEST_TAG=$REPO/$NAME:latest
+NPM_PACKAGE=$(cat package.json)
+export NAME=$(echo $NPM_PACKAGE | jq .name -r)
+export VERSION=$(echo $NPM_PACKAGE | jq .version -r)
+export REPO=$(echo $NPM_PACKAGE | jq .docker.repo -r)
+export CURRENT_TAG="$REPO/$NAME:$VERSION"
+export LATEST_TAG="$REPO/$NAME:latest"
